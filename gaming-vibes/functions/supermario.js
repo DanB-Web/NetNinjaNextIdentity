5c119985a38c649b3e3b353d863c1928e6e4af29
Dan <dan.bayford@gmail.com>
2021-05-11 10:03:03 +0100

    feat: function testing

--- gaming-vibes/functions/supermario.js
+++ gaming-vibes/functions/supermario.js
@@ -1,7 +1,16 @@
+/*NOTE SERVERLESS FUNCTIONS HAVE TO BE IN A FUNCTION FOLDER IN THE ROOT DIRECTORY.
+AND EACH FUNCTION HAS ITS OWN FILE*/ 
+
+/*YOU WILL GET A FUNCTION ENDPOINT FROM NETLIFY, AND YOU CAN TEST THE RETURN IN A BROWSER */
+/*WITHIN NETLIFY, YOU CAN SEE WHEN FUNCTIONS EXECUTE (THAT'S WHERTHE BELOW LOG WILL RUN*/
+
+/*https://deploy-preview-7--gaming-vibes-dab.netlify.app/.netlify/functions/supermario*/
+
 exports.handler = async () => {
+
   console.log('supermario function ran...');
 
-  //DUMMY DATA TO MOCK API CALL
+  //DUMMY DATA TO MOCK AN API CALL
   const data = { name: 'mario', age: '35', job: 'plumber'}
 
   //RETURN RESPONSE TO BROWSER
--- gaming-vibes/pages/guides.js
+++ gaming-vibes/pages/guides.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
+
+  useEffect(() => {
+    //NOTE THIS IS A RELATIVE PATH ON THE NETLIFY SERVER
+    fetch('/.netlify/functions/supermario')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
